5906a0186ea1ca8f245633288885d087af822468
jiiiyun <mia03232@gmail.com>
2023-10-27 14:04:48 +0900

    feat: 레시피 작성 페이지 생성

--- recipe-book/src/routes/Routing.jsx
+++ recipe-book/src/routes/Routing.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/auth/login/Login";
 import Signup from "../pages/auth/signup/Signup";
+import Postform from "../pages/post/Postform";
+import Postingredients from "../pages/post/Postingredients";
 
 function Routing() {
   return (
@@ -8,7 +10,8 @@ function Routing() {
       <Route path="/" element={<></>} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/posts/forms" element={<Postform />} />
+      <Route path="/postingredients" element={<Postingredients />} />
     </Routes>
   );
 }
